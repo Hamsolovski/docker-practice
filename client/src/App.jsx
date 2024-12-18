69c3c56d97f2ca0eb08e33f58c2f3632bdaaef13
Antoine <ant.delalande@gmail.com>
2024-12-18 11:29:20 +0100

    added hot reload on windows without wsl

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Test hot reloaad</h1>
+      <h1>Test hot reloaaaaaaad</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
